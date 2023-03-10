12c73d3072f0ed127d4610ea2eb423cf1ef7e282
alikeli <eli-com2@yandex.ru>
2023-03-10 15:50:33 +0100

    done

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength; j++) {
+                if (i >= j) {
+                    System.out.print("8");
+                }
+            }
+            System.out.println("");
+        }
 
+    }
+
+    public static void main(String[] args) {
+        Triangle t = new Triangle();
+        t.printTriangle(5);
     }
 }
